6afcb40b1aba761381dde6e2ab24493f256397e6
ashwin1014 <ashwin1014@yahoo.co.in>
2020-01-06 00:14:47 +0530

    added form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input/Input';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/Util/validators';
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                if(inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;                }
+            } 
+          return {
+              ...state,
+              inputs: {
+                  ...state.inputs,
+                  [action.inputId]: {value: action.value, isValid: action.isValid}
+              },
+              isValid: formIsValid
+          };
+        default:
+            return state;
+    }
+};
 
 const NewPlace = () => {
+
+    const [formState, dispatch] = React.useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+
+    const titleInputHandler = React.useCallback((id, value, isValid) => {
+   
+    }, []);
+
     return (
-        <div>
-            New Place
-        </div>
+        <form className='place-form'>
+            <Input label='Title' id='title' element='input' errorText='Please enter a valid title' validators={[VALIDATOR_REQUIRE()]} onInput={titleInputHandler} />
+            <Input label='Description' id='description' errorText='Please enter a valid description' validators={[VALIDATOR_MINLENGTH(5)]} onInput={titleInputHandler} />
+        </form>
     )
 }
 
