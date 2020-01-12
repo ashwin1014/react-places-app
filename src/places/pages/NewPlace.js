7030f586c4f193b8dc324c0685a393ec5bad8812
ashwin1014 <ashwin1014@yahoo.co.in>
2020-01-13 00:07:49 +0530

    form submit

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './NewPlace.css';
 import Input from '../../shared/components/FormElements/Input/Input';
+import Button from '../../shared/components/FormElements/Button/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/Util/validators';
 
 const formReducer = (state, action) => {
@@ -43,14 +44,21 @@ const NewPlace = () => {
         isValid: false
     });
 
-    const titleInputHandler = React.useCallback((id, value, isValid) => {
-   
+    const inputHandler = React.useCallback((id, value, isValid) => {
+        dispatch({ type: 'INPUT_CHANGE', value, isValid, inputId: id })
     }, []);
 
+    const placeSubmitHAndler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    };
+
     return (
-        <form className='place-form'>
-            <Input label='Title' id='title' element='input' errorText='Please enter a valid title' validators={[VALIDATOR_REQUIRE()]} onInput={titleInputHandler} />
-            <Input label='Description' id='description' errorText='Please enter a valid description' validators={[VALIDATOR_MINLENGTH(5)]} onInput={titleInputHandler} />
+        <form className='place-form' onSubmit={placeSubmitHAndler}>
+            <Input label='Title' id='title' element='input' errorText='Please enter a valid title' validators={[VALIDATOR_REQUIRE()]} onInput={inputHandler} />
+            <Input label='Description' element='textarea' id='description' errorText='Please enter a valid description' validators={[VALIDATOR_MINLENGTH(5)]} onInput={inputHandler} />
+            <Input label='Address'  element='input' id='address' errorText='Please enter a valid address' validators={[VALIDATOR_REQUIRE()]} onInput={inputHandler} />
+            <Button type='submit' disabled={!formState.isValid}>Add Place</Button> 
         </form>
     )
 }
